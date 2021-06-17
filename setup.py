c9046963bab3fffea4ac6a333f9dfa4fc01bf1e6
PARSH JAIN <66243977+immortal-zeus@users.noreply.github.com>
2021-06-18 00:34:01 +0530

    Version - 0.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md","r") as fh:
 
 setup(
     name = 'pjlog',
-    version = '0.0.6',
+    version = '0.0.8',
     description = 'Simple library for Logarithm with useful functions.',
     py_modules = ["pjlog"],
     package_dir = {'': 'src'},
